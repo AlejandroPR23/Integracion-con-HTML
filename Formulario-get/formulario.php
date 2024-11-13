b04d49459a3f69e042bba517a129ce3b6080e06d
AlejandroPR23 <perezrestrepoalejandro9@gmail.com>
2024-11-12 22:03:27 -0500

    💥 NEW: Add formulario con GET

---
+++ Formulario-get/formulario.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario con GET</title>
+</head>
+<body>
+    
+    <form action="./server.php" method="get">
+        <label for="nombre">Nombre:</label>
+        <input type="text" name="nombre" id="nombre" placeholder="Ingresa tu nombre">
+        <br>
+        <label for="edad">Edad:</label>
+        <input type="text" name="edad" id="nombre" placeholder="Ingresa tu edad">
+        <br>
+        <button type="submit">Enviar formulario🍻</button>
+    </form>
+
+</body>
+</html>
