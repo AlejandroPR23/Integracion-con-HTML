7c1efc24f22638ce9e0ae5c883b8f96c82284c04
AlejandroPR23 <perezrestrepoalejandro9@gmail.com>
2024-11-13 08:36:26 -0500

    💥 NEW: Add formulario con diferentes inputs

---
+++ Formulario diferentes inputs/server.php
@@ -0,0 +1,28 @@
+<?php
+
+echo "<pre>";
+print_r($_POST['nombre']);
+echo "</pre>";
+
+echo "<pre>";
+print_r($_POST['personas']);
+echo "</pre>";
+
+echo "<pre>";
+print_r($_POST['persona']);
+echo "</pre>";
+
+
+echo "<pre>";
+print_r($_POST['list1']);
+print_r($_POST['list2']);
+print_r($_POST['list3']);
+echo "</pre>";
+
+echo "<pre>";
+print_r($_POST['pais']);
+echo "</pre>";
+
+echo "<pre>";
+print_r($_FILES['galeria']);
+echo "</pre>";
--- Formulario imagen/formulario_imagen.php
+++ Formulario imagen/formulario_imagen.php
@@ -3,18 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario con POST</title>
+    <title>Formulario con imagenes</title>
 </head>
 <body>
     
-    <form action="./server.php" method="post">
+    <form action="./server.php" method="post" enctype="multipart/form-data">
         <label for="nombre">Nombre:</label>
         <input type="text" name="nombre" id="nombre" placeholder="Ingresa tu nombre">
         <br>
-        <label for="edad">Edad:</label>
-        <input type="text" name="edad" id="nombre" placeholder="Ingresa tu edad">
+        <label for="image">image:</label>
+        <input type="file" name="image" id="nombre">
         <br>
-        <button type="submit">Enviar formulario🍻</button>
+        <button type="submit">Enviar formulario🧁🍹🍩</button>
     </form>
 
 </body>
